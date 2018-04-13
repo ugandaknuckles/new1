369a6b9921e380be7d09e870938f83ac28dbfaa4
ugandaknuckles <35495803+ugandaknuckles@users.noreply.github.com>
2018-04-13 09:05:34 -0600

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,31 +1,49 @@
 #include<iostream>
 #include<Windows.h>
+#include<ctime>
 using namespace std;
+
+
+//function declaration
+char disaster();
+
+
 int main() {
-
-	int song;
-	
+srand(time(NULL));
 	while (1) {
-		cout << "what song do you want?" << endl;
-		cin >> song;
-		//system("pause");
-		switch (song) {
+		cout << " A rando disaster is coming " << endl;
+		
+		//function call
+		cout << " you got a" << disaster()<<endl;
+		system("pause");
 
 
-		case 1:
-			cout << "song fortnie" << endl;
-			PlaySound(TEXT("fortnite.wav.wav"), NULL, SND_FILENAME);
-			break;
-		case 2:
-			cout << " song MCfallen" << endl;
-			PlaySound(TEXT("recycle.wav"), NULL, SND_FILENAME);
-			break;
-		case 3:
-			cout << "song man" << endl;
-			PlaySound(TEXT("recycle.wav"), NULL, SND_FILENAME);
-			break;
 
+	}//end game loop
+}//end main
 
-		}
+//function definition
+char disaster() {
+	int num = rand() % 100 + 1;
+
+	if (num > 10) {
+		cout << "fire";
+		return 'f';
 	}
+	else if (num > 25) {
+		cout << "radroach";
+		return 'r';
+	}
+	else if (num > 37) {
+		cout << "raider attack";
+		return 'ra';
+	}
+	else if (num > 40) {
+		cout << " mole rat";
+		return 'm';
+	}
+	else
+		cout << "enchilada attack ";
+	return 'n';
 }
+	
